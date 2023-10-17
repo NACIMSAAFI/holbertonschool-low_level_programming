559aed90eb825d0edb5dd1f16c357ac47d1aa0ae
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-17 10:14:07 +0100

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	char ch = 'a';
+char ch = 'a';
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
-    for (ch = 'A'; ch <= 'Z'; ch++)
-    putchar(ch);
-    putchar('\n');
-	return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }
