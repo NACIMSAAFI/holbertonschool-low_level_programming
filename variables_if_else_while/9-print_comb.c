90ae9b68bbeacc3dad8131eb7e107643476ec0fe
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-17 19:32:09 +0100

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Return:0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Return:0
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 0; ch <= 9; ch++)
+{
+        putchar(ch + '0');
+        if (ch < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+putchar('\n');
+return (0);
+}
