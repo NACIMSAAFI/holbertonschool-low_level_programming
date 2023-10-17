2c268ced662fc2a97c7eabc9dda4facee6be7b4f
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-17 19:42:15 +0100

    9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -20,13 +20,13 @@ int ch;
 
 for (ch = 0; ch <= 9; ch++)
 {
-        putchar(ch + '0');
-        if (ch < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+putchar(ch + '0');
+if (ch < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
