a2ee54074d23c087e5ec120cf1900a337bd7346a
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-18 12:18:06 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+char ch = 'a';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+_putchar('\n');
+}
