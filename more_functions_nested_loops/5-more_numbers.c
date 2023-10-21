83ef776b6644889d273260528d82f59036657158
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-21 19:43:03 +0100

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers -Fonction that preints *10 from 0 to 14, followed by a new line.
+ */
+void more_numbers(void)
+{
+	int c;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
