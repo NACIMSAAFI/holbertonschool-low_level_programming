c3c232b885b90d2fc143cfd0003bea4d12665208
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-21 15:46:29 +0100

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - Function that cmultiplies two integers.
+ * Return: a*b
+ */
+void print_numbers(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
