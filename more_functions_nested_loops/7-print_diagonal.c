34bff5628a9b0b15f2bc6c7077e4673d554d22a3
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-21 23:49:46 +0100

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,34 +10,17 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		if (n <= 0)
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
 			_putchar('\n');
 		}
-		else
-		{
-			for (int i = 0; i < n; i++)
-			{
-				for (int j = 0; j < i; j++)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
-			}
-		}
 	}
-}
-else
-{
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('\n');
-	}
-}
 }
