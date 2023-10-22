eacf3a858baed47b9a12b5e5e125f54625bd54fd
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-22 10:46:02 +0100

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - prints the numbers from 1 to 100 with FizzBuzz conditions
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d \n", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
