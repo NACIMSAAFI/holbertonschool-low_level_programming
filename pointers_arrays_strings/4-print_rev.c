ce6b59c1a83a193a83727e03d6b86d83b49246cd
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-24 10:55:05 +0100

    print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,12 @@
 
 void print_rev(char *s)
 {
-    int i;
-    int len = strlen(s);
+	int i;
+	int len = strlen(s);
 
-    for (i = len - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
