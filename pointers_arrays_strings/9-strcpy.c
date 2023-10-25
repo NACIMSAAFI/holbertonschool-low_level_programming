7b6eb654b22418c69beac2e1dc8a1d7e4de780e1
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-25 12:09:57 +0100

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ **_strcpy - Function that copies a string from src to dest.
+ *Return: A pointer to the destination string.
+ *@src: Source string
+ *@dest: Destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
