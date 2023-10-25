8f88f386d36586e7547cb20561dbe94dc20ce856
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-25 14:03:07 +0100

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	unsigned int i ;
-	while (src[i] != '\0')
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest+i)=*(src+i);
-		i++;
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
