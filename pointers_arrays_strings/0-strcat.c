b5c6a67bfcdd9a480239419849aa4e5ccf57153a
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-26 14:10:08 +0100

    #include "main.h" /**  **_strcpy - Function that concatenates two strings  *Return: A pointer to the destination string.  *@src: Source string  *@dest: Destination string.  */ char *_strcat(char *dest, char *src) { 	int i, j; for (j = 0; dest[j] != 0; j++) { } 	for (i = 0; src[i] != 0; i++) 	{ 	dest[j+1]=src[i]; 	} 	dest[i] = 0;
    	return (dest);
    }
    
    ./0-strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ **_strcpy - Function that concatenates two strings
+ *Return: A pointer to the destination string.
+ *@src: Source string
+ *@dest: Destination string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+for (j = 0; dest[j] != '\0'; j++)
+{
+}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[j]=src[i];
+    j++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
