7a1dcfa95104e481982185fe29e3658027b0d3b3
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-26 09:15:51 +0100

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - Function that converts a string to an integer.
+ * Return: The integer value extracted from the string.
+ * @s: The input string.
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int result = 0;
+	int sign = 1;
+	int found_digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			break;
+		}
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			found_digit = 1;
+		}
+		else if (found_digit)
+		{
+			break;
+		}
+		i++;
+	}
+
+	return (sign * result);
+}
