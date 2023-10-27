3c8ee2019a8bf1f3647d01251d28eedba6e288bc
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-27 20:54:09 +0100

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,19 +8,25 @@
 char *cap_string(char *str)
 {
 	int i;
-	 if (str[0] >= 97 && str[0] <= 122) {
-        str[0] = str[0] - 32;
-    }
+
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] = str[0] - 32;
+	}
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 97 && str[i] <= 122) &&
-		(str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-		|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
-		|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
-		|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-		|| str[i - 1] == '}'))
-		
+		if ((str[i] >= 97 && str[i] <= 122)
+		&& (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' || str[i - 1] == '!'
+		|| str[i - 1] == '?' || str[i - 1] == '"'
+		|| str[i - 1] == '(' || str[i - 1] == ')'
+		|| str[i - 1] == '{' || str[i - 1] == '}'))
+
 			str[i] = str[i] - 32;
 	}
 	return (str);
