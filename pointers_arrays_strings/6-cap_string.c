51fb159de802051a645a4f248c05e02b8dc63762
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-27 20:29:34 +0100

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -5,15 +5,25 @@
  *Return: A pointer to the new string.
  *@str: String to change
  */
-char *cap_string(char *str)
-{
-	int i;
+char *cap_string(char *str) {
+    int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if ((str[i] >= 97 && str[i] <= 122) && (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}'))
 
-			str[i] = str[i] - 32;
-	}
-	return (str);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\t' || str[i] == '\n') {
+            str[i] = ' ';
+        }
+    }
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if ((str[i] >= 'a' && str[i] <= 'z') &&
+            (i == 0 || str[i - 1] == ' ' || str[i - 1] == ',' ||
+             str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
+             str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' ||
+             str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')) {
+            str[i] = str[i] - 32;
+        }
+    }
+    return str;
 }
+
