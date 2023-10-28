95618385c295a8d6598670a2250f77ae4d5e812c
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-28 19:45:22 +0100

    Function that fills memory with a constant byte correction return

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,9 +11,12 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
+	{ if (s[i] != '\0') {
+		s[i] = b;}
+	else {
+			break;
+		}
 	}
 
-	return (0);
+	return (s);
 }
