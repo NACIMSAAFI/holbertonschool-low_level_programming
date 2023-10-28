4f89fb6d0740838ab97c3aae03855a6a63fbfea9
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-28 19:52:43 +0100

    Function that fills memory with a constant byte elimination of si !=0 condition

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,12 +11,8 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{ if (s[i] != '\0') {
-		s[i] = b;}
-	else {
-			break;
-		}
-	}
+	
+		s[i] = b;
 
 	return (s);
 }
