98e57a88b62cbe8d61f5f029aa9a2cebe9536397
NACIMSAAFI <7715@holbertonstudents.com>
2023-10-29 15:35:43 +0100

    2-strchr modification Betty style

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,24 @@
 #include "main.h"
-#include <stddef.h> 
+#include <stddef.h>
 /**
  * _strchr - Function that locates a character in a string.
- * Return: Pointer to the first occurrence of the character in the string, or "nil" if not found.
+ * Return: Pointer to the first occurrence of
+ * the character in the string, or "nil" if not found.
  * @s: The string to search.
  * @c: The character to locate.
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return s;
-        s++;
-    }
-    
-    if (*s == c)
-        return s;
-    
-    return NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == c)
+		return (s);
+
+	return (NULL);
 }
 
