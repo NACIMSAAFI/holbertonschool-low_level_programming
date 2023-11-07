04424404ed4f32efd19e683fee8cfe0b64eb199b
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-07 10:30:51 +0100

    2-str_concat.c function that concatenates tow strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,41 +12,39 @@
  * followed by the contents of s2, and null terminated,
  * or NULL if insufficient memory was available.
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 char *str_concat(char *s1, char *s2)
 {
 	char *concatstr;
-	int len1, len2, i, j;
+	unsigned int len1, len2, i, j;
 
-	if (s1 == NULL)
-	s1 = '\0';
-	len1 = 1;
-	if (s2 == NULL)
-	s2 = '\0';
-	len2 = 1;
-	{
-		return (NULL);
-	}
+    if (s1 == NULL) {
+        s1 = ""; 
+    }
+    if (s2 == NULL) {
+        s2 = ""; 
+    }
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+    len1 = strlen(s1);
+    len2 = strlen(s2);
 	concatstr = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+    if (concatstr == NULL) {
+        return NULL; 
+    }
+    
+    for (i = 0; i < len1; i++) {
+        concatstr[i] = s1[i];
+    }
 
-	if (concatstr == NULL)
-	{
-		return (NULL);
-	}
+    for (j = 0; j < len2; j++) {
+        concatstr[i + j] = s2[j];
+    }
 
-	for (i = 0; i < len1; i++)
-	{
-		concatstr[i] = s1[i];
-	}
+    concatstr[i + j] = '\0'; 
 
-	for (j = 0; j < len2; j++)
-	{
-		concatstr[i + j] = s2[j];
-	}
-
-	concatstr[i + j] = '\0';
-
-	return (concatstr);
+    return concatstr;
 }
+
