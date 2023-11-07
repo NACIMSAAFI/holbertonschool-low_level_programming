b42d5bb4b29f93333cd8a281fda9d01a198a2c95
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-07 08:48:08 +0100

    final

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -20,11 +20,11 @@ char *_strdup(char *str)
 	int j;
 	char *newstr;
 
-	if(str==NULL)
-		return(NULL);
+	if(str == NULL)
+		return (NULL);
 	
-	len=strlen(str);
-	newstr = malloc(len * sizeof(char)+1);
+	len = strlen(str);
+	newstr = malloc(len * sizeof(char) + 1);
 	if (newstr == NULL)
 		return (NULL);
 
