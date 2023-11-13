13e131ca84862e7da2fec94dd880e7d707507c81
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-13 11:12:44 +0100

    dog.h a new type to dfine

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define _DOG_H
 
 /**
- * struct dog - a new type to dfine.
+ * struct dog - a new type to define.
  * @name: type char *
  * @age: type float
  * @owner: type char *
  */
-
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
+
 #endif
