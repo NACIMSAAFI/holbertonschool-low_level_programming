f36804721cdee531c2aed4ba0a4e710e7d0a57ab
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-14 13:26:53 +0100

    Add void array_iterator(int *array, size_t size, void (*action)(int)) to function_pointers.h

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
- * array_iterator - function that executes a function given as a parameter
+ * array_iterator - function that executes a function given as a parameter.
  * on each element of an array.
  * @array: array with elements.
  * @size: size of the array.
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != 0)
+	if (action != 0 && array !=0)
 	{
 		for (i = 0; i < size; i++)
 		{
