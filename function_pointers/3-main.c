a6064d481504078f95ee06c3752b064dc7887015
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-16 21:37:26 +0100

    3 program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,20 +20,19 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (98);
 	}
-	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0
-	&& strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") != 0
-	&& strcmp(argv[2], "%") != 0)
+	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") != 0 && strcmp(argv[2], "%") != 0)
 	{
 		printf("Error\n");
 		return (99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0 && num2 == 0))
+	if ((strcmp(argv[2], "/") == 0 && num2 == 0) || (strcmp(argv[2], "%") == 0 && num2 == 0))
 	{
 		printf("Error\n");
 		return (100);
 	}
+
 	operation_result = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", operation_result);
 
