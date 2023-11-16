4aeabb3609412fc1fc5f2e5444363a2973ad4ebe
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-16 21:20:16 +0100

    3 program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -34,6 +34,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (100);
 	}
+	if (strcmp(argv[2], "%") == 0 && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 	operation_result = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", operation_result);
 
