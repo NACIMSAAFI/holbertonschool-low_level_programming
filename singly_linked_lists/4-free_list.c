5e337e4664f354226b716ef5383a41090a630e71
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-29 20:33:57 +0100

    4-free_list.c Function that frees the memory allocated for a linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees the memory allocated for a linked list.
+ * @head: A pointer to the head of the linked list.
+ * Parameters:
+ *   head: A pointer to the head of the linked list.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
