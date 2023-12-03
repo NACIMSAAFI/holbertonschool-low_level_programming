686f0d9ce702227145d4265a7f89122f745390a6
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-03 11:13:45 +0100

    7-Inserts a new node  * at a given position in a doubly linked list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
 /**
  * insert_dnodeint_at_index - Inserts a new node
- * at a given position in a doubly linked list.
- * @h: Pointer to the pointer to the head of the linked list.
+ * @h: Pointer to the pointer to the head.
  * @idx: Index at which the new node should be inserted.
  * @n: Integer value to be stored in the new node.
- *
  * Return: Pointer to the new node, or NULL on failure.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
