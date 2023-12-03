dc489485e3328076ca132e0221a7b3ea4eb8b34d
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-03 18:16:47 +0100

    2-get_bit.c Function gets the value of a bit at a given index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -5,13 +5,11 @@
  * @n: The number to extract the bit from.
  * @index: The index of the bit to get.
  *
- * Return: The value of the bit at the specified index,
- * or -1 if an error occurs.
+ * Return: The value of the bit at the specified index, or -1 if an error occurs.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned int len = 0; 
-    unsigned int i;
+    unsigned int len = 0;
 
     while ((n >> len) > 0)
         len++;
@@ -19,11 +17,5 @@ int get_bit(unsigned long int n, unsigned int index)
     if (index >= len)
         return (-1);
 
-    for (i = 0; i < len; i++)
-    {
-        if (i == index)
-            return ((n >> i) & 1); 
-    }
-
-    return (-1); 
+    return ((n >> index) & 1);
 }
