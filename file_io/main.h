3ac902f7a8e79b2a09e54b7032129f3582ed2d21
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-05 09:31:30 +0100

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
