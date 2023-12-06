4546bdaa915032d6cd11925f8115817fca251288
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-06 10:34:20 +0100

    2-append_text_to_file.c Function apprends text to the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,22 +14,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file;
+	int file;
 
-    if (filename == NULL)
-    return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file = open(filename, O_WRONLY | O_APPEND);
-    if (file == -1)
-    {
-    return (-1);
-    }
-    if (text_content == NULL)
-    {
-        return(1);
-    }
-  write(file, text_content, strlen(text_content));
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	write(file, text_content, strlen(text_content));
 
-    close (file);
-    return(1);
+	close(file);
+	return (1);
 }
