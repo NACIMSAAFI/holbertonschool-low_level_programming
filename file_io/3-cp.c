ce2489e8c4a958079a21b0bae9ad27ebc2d5f8de
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-07 11:29:53 +0100

    3-cp.c program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - Entry point for the cp program,
  * copies the content of a file to another file.
@@ -10,43 +11,52 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[10000];
+    int file_from, file_to;
+    ssize_t bytes_read, bytes_written;
+    char buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp %s %s\n", argv[1], argv[2]);
-		exit(97);
-	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		return (-1);
-	}
-	bytes_read = read(file_from, buffer, 10000);
-	if (bytes_read == -1)
-	{
-		dprintf(2, "Error: Can't read from file%s\n", argv[1]);
-		exit(98);
-	}
-	close(file_from);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-	{
-		return (-1);
-	}
-	bytes_written = write(file_to, buffer, bytes_read);
-	if (bytes_written == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	close (file_to);
+    if (argc != 3)
+    {
+        dprintf(2, "Usage: %s <file_from> <file_to>\n", argv[0]);
+        exit(97);
+    }
+
+    file_from = open(argv[1], O_RDONLY);
+    if (file_from == -1)
+    {
+        return (-1);
+    }
+
+    bytes_read = read(file_from, buffer, sizeof(buffer));
+    if (bytes_read == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+        close(file_from);
+        exit(98);
+    }
+
+    close(file_from);
+
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (file_to == -1)
+    {
+        return (-1);
+    }
+
+    bytes_written = write(file_to, buffer, bytes_read);
+    if (bytes_written == -1)
+    {
+        dprintf(2, "Error: Can't write to %s\n", argv[2]);
+        close(file_to);
+        exit(99);
+    }
 	if (close(file_from) == -1 || close(file_to) == -1)
 	{
 		dprintf(2, "Error: Can't close %s or %s\n", argv[1], argv[2]);
 		exit(100);
 	}
-	return (0);
+
+    close(file_to);
+
+    return (0);
 }
