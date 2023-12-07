9a0b9636c006c36c030ac7ed1b5986d66df73dc7
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-07 13:27:07 +0100

    3-cp.c program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,12 +1,9 @@
 #include "main.h"
-
 /**
  * main - Entry point for the cp program,
  * copies the content of a file to another file.
- *
  * @argc: The number of command-line arguments.
  * @argv: An array containing the command-line arguments.
- *
  * Return: 0 on success, or an exit code indicating an error.
  */
 int main(int argc, char *argv[])
@@ -20,35 +17,30 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
-
 	bytes_read = read(file_from, buffer, sizeof(buffer));
 	if (bytes_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(file_from);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	bytes_written = write(file_to, buffer, bytes_read);
 	if (bytes_written == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_to);
 		exit(99);
 	}
-
 	if (close(file_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %s\n", argv[1]);
+		dprintf(STDOUT_FILENO,, "Error: Can't close fd %s\n", argv[1]);
 		exit(100);
 	}
 	if (close(file_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %s\n", argv[2]);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %s\n", argv[2]);
 		exit(100);
 	}
 	return (0);
