bfb9cbc37d717bc351e37c7aac785e7f91d64895
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-07 14:13:17 +0100

    3-cp.c program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,21 +17,23 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	file_from = open(argv[1], O_RDONLY);
-	bytes_read = read(file_from, buffer, sizeof(buffer));
+	while ((bytes_read = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close(file_to);
+			exit(99);
+		}
+	}
 	if (bytes_read == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(file_from);
 		exit(98);
-	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	bytes_written = write(file_to, buffer, bytes_read);
-	if (bytes_written == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		close(file_to);
-		exit(99);
 	}
 	if (close(file_from) == -1)
 	{
