9ec7b2ef22bde4c56f572d2cb78b4c8751eb4324
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-10 19:47:36 +0100

    3-hash_table_set.c function that adds an element to the hash tabl

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,79 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds a key-value pair to a hash table.
+ *
+ * @ht: The hash table to which the key-value pair should be added.
+ * @key: The key (string) associated with the value.
+ * @value: The value (string) to be stored.
+ *
+ * Return:
+ *   -  0: Success
+ *   - -1: Memory allocation failure
+ *   - -2: Key already exists in the hash table
+ *   - -3: Invalid arguments (hash table, key, or value is NULL)
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index;
+	hash_node_t *new_node = NULL;
+	hash_node_t *current_node = NULL;
+
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (-3);
+	}
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current_node = lookup_data_string_(ht, key);
+	if (current_node != NULL)
+	{
+		return (-2);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (-1);
+	}
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (0);
+}
+
+/**
+ * lookup_data_string_ - Looks up a key
+ * in a hash table and returns the associated data (value).
+ *
+ * @ht: The hash table to search.
+ * @key: The key to look up.
+ *
+ * Return:
+ *   - pointer to the value associated with the key if found.
+ *   - NULL if the key is not found.
+ */
+hash_node_t *lookup_data_string_(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *current_node;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			return (current_node);
+		}
+		current_node = current_node->next;
+	}
+
+	return (NULL);
+}
