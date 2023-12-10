16713c1ca181d8c0370a9c8628ed873b17bdae6b
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-10 20:02:36 +0100

    3-hash_table_set.c Function that gives the index for a given key in hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 	{
-		return (0);
+		return (1);
 	}
 
 	index = key_index((unsigned char *)key, ht->size);
