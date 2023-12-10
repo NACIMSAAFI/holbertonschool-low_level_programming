bbad46f6d5fe5327e2484687d7784cd6144136b0
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-10 19:49:13 +0100

    update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -40,5 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+hash_node_t *lookup_data_string_(const hash_table_t *ht, const char *key);
+
 
 #endif
