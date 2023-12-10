12f87f120c48c74e10f5f3109fa44643440b6956
NACIMSAAFI <7715@holbertonstudents.com>
2023-12-10 20:39:04 +0100

    3-hash_table_set.c Function that gives the index for a given key in hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,54 +11,46 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned int index;
-    hash_node_t *new_node = NULL;
-    hash_node_t *current_node = NULL;
+	unsigned int index;
+	hash_node_t *new_node = NULL;
+	hash_node_t *current_node = NULL;
 
-    if (ht == NULL || key == NULL || value == NULL)
-    {
-        return (0);
-    }
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((unsigned char *)key, ht->size);
 
-    index = key_index((unsigned char *)key, ht->size);
+	current_node = lookup_data_string_(ht, key);
+	if (current_node != NULL)
+	{
+		free(current_node->value);
+		current_node->value = strdup(value);
+		if (current_node->value == NULL)
+		{
+			return (0);
+		}
+		return (1);
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    current_node = lookup_data_string_(ht, key);
-    if (current_node != NULL)
-    {
-        free(current_node->value);
-        current_node->value = strdup(value);
-        if (current_node->value == NULL)
-        {
-            return (0); 
-        }
-        return (1); 
-    }
-
-    
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-    {
-        return (0);  
-    }
-
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-
-    if (new_node->key == NULL || new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node->value);
-        free(new_node);
-        return (0);  
-    }
-
- 
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return (1);  
+	return (1);
 }
-
 /**
  * lookup_data_string_ - Looks up a key
  * in a hash table and returns the associated data (value).
